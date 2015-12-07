635cf034366dd07c97447879f5d3c55928aeadb1
Eduardo <edu.ugolini2@hotmail.com>
2015-12-07 09:04:22 -0200

    Adicionadas classes que ficaram de fora no ultimo commit

---
+++ FileManager.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifcvideira.trampo;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author eduardo
+ */
+public class FileManager {
+    public static void saveInformations(String content) throws Exception{
+        String filePath = System.getProperty("user.home") + "/ultrasoft.txt";
+        File file = new File(filePath);
+        
+        if(!file.exists()){
+            file.createNewFile();
+        }
+        
+        FileWriter fw = new FileWriter(file);
+        try (BufferedWriter bw = new BufferedWriter(fw)) {
+            ArrayList<String> linhas = FileManager.getInformations();
+            
+            for(String linha : linhas){
+                bw.newLine();
+                bw.write(linha);
+            }
+            bw.newLine();
+            bw.write(content);
+        }
+    }
+    
+    public static ArrayList getInformations() throws Exception{
+        ArrayList<String> data = new ArrayList<>();
+        
+        String filePath = System.getProperty("user.home") + "/ultrasoft.txt";
+        File file = new File(filePath);
+        
+        if(!file.exists()){
+            throw new Exception("Deu ruim, não tem nada para ver aqui!");
+        }
+        
+        FileReader fr = new FileReader(file);
+        try (BufferedReader br = new BufferedReader(fr)){
+            String line;
+            while ((line = br.readLine()) != null) {
+                data.add(line);
+            }
+        }
+        
+        return data;
+    }
+}
